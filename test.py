781b5e191bcea88b67f5682ea134f09673bbc90f
harris <harryaamir073@gmail.com>
2024-03-30 16:44:41 +0500

    testWorkflow

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ class TestMyFunctions(unittest.TestCase):
         self.assertEqual(getLabel("Credit card number is not valid"), "spam") 
         self.assertEqual(getLabel("Amount credited with 1000$"), "spam") 
         self.assertEqual(getLabel("New account created"), "ham") 
+        self.assertEqual(getLabel("your account was deactivated "), "spam") 
         
 if __name__ == '__main__':
     unittest.main()
