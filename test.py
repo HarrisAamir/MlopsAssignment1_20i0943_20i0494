18bcae7c9aa9cc0c8252ca9756772dc142da8a78
AshishJumani <114658349+AshishJumani@users.noreply.github.com>
2024-03-30 17:45:06 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import unittest
 from model import getLabel
 
+
 class TestMyFunctions(unittest.TestCase):
 
     def test_add_numbers(self):
-        self.assertEqual(getLabel("Credit card number is not valid"), "spam") 
-        self.assertEqual(getLabel("Amount credited with 1000$"), "spam") 
-        self.assertEqual(getLabel("New account created"), "ham") 
-        self.assertEqual(getLabel("your account was deactivated "), "spam") 
-        
+        self.assertEqual(getLabel("Credit card number is not valid"), "spam")
+        self.assertEqual(getLabel("Amount credited with 1000$"), "spam")
+        self.assertEqual(getLabel("New account created"), "ham")
+        self.assertEqual(getLabel("your account was deactivated"), "spam")
+
+
 if __name__ == '__main__':
     unittest.main()
